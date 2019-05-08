c746ca889901892a17cb45dd7514fafb8320e58b
Ваше Имяvahan024124 <you@example.com>
2019-05-08 23:55:48 +0400

    Json

---
+++ Vahan Abovyan/json.js
@@ -0,0 +1,95 @@
+{
+"armliter":{
+	"vahagn_davtyan":{
+                "genre": " ",
+		"img": " ",	 	
+		"author": "Վահագն_Դավթյան",
+		"bookename": "Ամառային_ամպրոպ"
+},
+	
+	"metaksim":{
+                "genre": " ",
+		"img": " ",	 	
+		"author": "Մետակսիմ",
+		"bookename": "Սերը _հավատ է"
+},
+
+
+	"vardan_aygekci":{
+                "genre": " ",
+		"img": " ",	 	
+		"author": "Վարդան_Այգեկցի",
+		"bookename": "Բարի_գործը"
+},
+
+	"hovhanes_shiraz":{
+                "genre": " ",
+		"img": " ",	 	
+		"author": "Հովհաննես_Շիրազ",
+		"bookename": "Գարնանամուտ"
+},
+}
+
+{
+"rusliter":{
+	
+	"lev_tolstoy":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Лев_Толстой",
+			"bookename": "Анна_Каренина"
+"
+	},
+		
+		"nikolay_gogl":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Николай_Гоголь",
+			"bookename": "Шинель"
+"
+	},
+
+
+		"ivan_bunin":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Иван_Бунин",
+			"bookename": "Тёмные_аллеи"
+"
+	},
+
+		"vladimir_nabokov":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Владимир_Набоков ",
+			"bookename": "Лолита"
+"
+	},
+	}
+
+{
+"Geography":{
+	
+	        "lev_tolstoy":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Влодавец_В.И",
+			"bookename": "Вулканы_Земли"
+
+	},
+		
+}
+
+{
+"psychology":{
+	
+	        "nalchajyan":{
+		        "genre": " ",
+			"img": " ",	 	
+			"author": "Նալչաջյան_Ա.Ա",
+			"bookename": "Հոգեբանություն"
+
+	},
+		
+}
+
